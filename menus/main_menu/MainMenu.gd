f0b8bc79f3244820f456c3d02d7ff18b4ad1d1de
Alex Warnes <warnes.alexander@gmail.com>
2023-12-03 19:55:29 -0800

    Working on the menu

--- menus/main_menu/MainMenu.gd
+++ menus/main_menu/MainMenu.gd
@@ -1,7 +1,5 @@
 extends Control
 
-
-
 func _on_quit_button_pressed():
 	get_tree().quit()
 
@@ -9,4 +7,4 @@ func _on_settings_button_pressed():
 	Global.goto_scene("res://menus/settings_menu/SettingsMenu.tscn")
 
 func _on_play_button_pressed():
-	print("Play Pressed!")
+	Global.goto_scene("res://game/Planning.tscn")
