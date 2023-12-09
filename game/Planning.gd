7dc45acd8561214536132198461ffaabc94b8b41
Alexander Warnes <warnes.alexander@gmail.com>
2023-12-09 06:49:24 -0800

    Adding planning and selection screens

--- game/Planning.gd
+++ game/Planning.gd
@@ -1,6 +1,10 @@
 extends Control
 
-
-
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_food_button_pressed():
+	# get resource
+	# load selection screen with resource
+	pass
---
+++ game/selection_screen/resources/FoodSelectionScreen.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name FoodSelectionScreen
+
+const title = 'Food and Recipes'
+
